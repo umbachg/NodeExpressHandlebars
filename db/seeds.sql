a2cc95dde4f9e7ef345e101d658a52b6b5f8c782
Gabrielle Umbach <g.umbach8098@gmail.com>
2019-12-16 10:01:21 -0600

    ..

---
+++ db/seeds.sql
@@ -0,0 +1,12 @@
+INSERT INTO burgers
+    (burger_name, devoured)
+VALUES
+    ('beef Burger', FALSE);
+INSERT INTO burgers
+    (burger_name, devoured)
+VALUES
+    ('turkey burger', FALSE);
+INSERT INTO burgers
+    (burger_name, devoured)
+VALUES
+    ('chicken Burger', FALSE);
